259f10a7686f805c9f7cd5a0373cd99716d9233f
Hp <Hp@HIEUNGUYEN>
2024-07-08 21:56:28 +0700

    complete lession 45

--- todoapp/todoapp/src/components/Todo.jsx
+++ todoapp/todoapp/src/components/Todo.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Form from "./Form";
 import TodoList from "./TodoList";
+import Footer from "./Footer";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.done).length;
   return (
     <div>
       <Form todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} />
+      <Footer completedTodos={completedTodos} totalTodos={todos.length} />
     </div>
   );
 }
--- todoapp/todoapp/src/components/TodoItem.jsx
+++ todoapp/todoapp/src/components/TodoItem.jsx
@@ -1,8 +1,33 @@
 import styles from "./todoitem.module.css";
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+  function handleClick(name) {
+    const newTodos = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newTodos);
+  }
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        <span
+          className={item.done ? styles.completed : ""}
+          onClick={() => handleClick(item.name)}
+        >
+          {item.name}
+        </span>
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
